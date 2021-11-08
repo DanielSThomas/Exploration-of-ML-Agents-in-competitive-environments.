897ff7239de53422e4305d4fcaffd038927aa61e
Daniel Thomas <dansebtom@yahoo.co.uk>
2021-11-08 01:59:50 +0000

    Create in editor functionality to load an external ml-agent brain

--- MLArena/Assets/Scripts/LoadBrain.cs
+++ MLArena/Assets/Scripts/LoadBrain.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.MLAgents;
+//using UnityEditor;
+using System.IO;
 using Unity.MLAgents.Policies;
 using Unity.Barracuda;
+using Unity.MLAgents;
+using Unity.Barracuda.ONNX;
 
 public class LoadBrain : MonoBehaviour
 {
-    [SerializeField] private BehaviorParameters bp;
-    [SerializeField] private NNModel loadedBrain;
 
+    
+
+    private BehaviorParameters bp;
+
+    private string path;
+
+    private Agent agent;
 
     // Start is called before the first frame update
     void Start()
     {
         bp = GetComponent<BehaviorParameters>();
+        agent = GetComponent<Agent>();
     }
 
     // Update is called once per frame
@@ -23,11 +32,48 @@ public class LoadBrain : MonoBehaviour
         
     }
 
-    private void Loadbrain()
+
+
+    //credit https://github.com/Unity-Technologies/ml-agents/blob/0.14.1/Project/Assets/ML-Agents/Examples/SharedAssets/Scripts/ModelOverrider.cs#L114-L128
+    public void Loadbrain()
     {
 
+        //string path = EditorUtility.OpenFilePanel("Load Brain", "", "onnx");
 
 
-        bp.Model = loadedBrain;
+        byte[] model = null;
+
+        try
+        {
+            model = File.ReadAllBytes(path);
+        }
+        catch (IOException)
+        {
+            Debug.Log($"Couldn't load file {path}", this);
+                    
+        }
+        //Apparently loading a onnx file to unity in realtime is a pain... This code is mostly from Unity's own example from the link above.
+        var converter = new ONNXModelConverter(true);
+        var onnxModel = converter.Convert(model);
+
+        NNModelData assetData = ScriptableObject.CreateInstance<NNModelData>();
+        using (var memoryStream = new MemoryStream())
+        using (var writer = new BinaryWriter(memoryStream))
+        {
+            ModelWriter.Save(writer, onnxModel);
+            assetData.Value = memoryStream.ToArray();
+        }
+       
+        var asset = ScriptableObject.CreateInstance<NNModel>();
+        asset.modelData = assetData;
+        asset.name = "Imported Brain";
+
+
+        agent.SetModel(bp.BehaviorName, asset);
+
+
     }
+
+    
+
 }
