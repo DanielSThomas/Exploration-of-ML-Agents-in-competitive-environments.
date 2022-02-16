3f73ada7755bf3c2e62850ca40bbdbba7155c6cc
Daniel Thomas <dansebtom@yahoo.co.uk>
2022-02-16 22:27:41 +0000

    Update Basic Level design and bullet colours

--- MLArena/Assets/Scripts/Elimination/Bullet.cs
+++ MLArena/Assets/Scripts/Elimination/Bullet.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class Bullet : MonoBehaviour
 {
@@ -11,30 +12,34 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private EliminationAgent bulletOwner;
     [SerializeField] SpriteRenderer sr;
+    [SerializeField] Light2D li;
 
 
     private void Start()
     {
+        li = GetComponent<Light2D>();
         sr = GetComponent<SpriteRenderer>();
 
         if (bulletTeam == 0)
         {
             this.tag = "RedBullet";
             this.gameObject.layer = 12;
-            sr.color = Color.red;
+            sr.color = new Color(1.0f, 0.30f, 0.0f); //orange
+            li.color = new Color(1.0f, 0.30f, 0.0f);
         }
         else if (bulletTeam == 1)
         {
             this.tag = "BlueBullet";
             this.gameObject.layer = 11;
-            sr.color = Color.blue;
+            sr.color = Color.cyan;
+            li.color = Color.cyan;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.up * speed;
+        transform.position += transform.up * speed * Time.deltaTime;
 
         bulletLifeTime -= 1;
         if (bulletLifeTime <= 0)
